ee102ca2056cbe8e4a5411106a9a23bd1905b028
Kyle Jacobs <vaultman765@gmail.com>
2021-06-05 09:02:37 -0400

    Style updates

--- First_Project/mysite/food/urls.py
+++ First_Project/mysite/food/urls.py
@@ -4,12 +4,12 @@ from django.urls import path
 app_name = 'food'
 urlpatterns = [
     # /food/
-    path('', views.index, name='index'),
+    path('', views.IndexClassView.as_view(), name='index'),
     #/food/1
     path('item/', views.item, name='item'),
-    path('<int:item_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.FoodDetail.as_view(), name='detail'),
     # add items
-    path('add', views.create_item, name='create_item'),
+    path('add', views.CreateItem.as_view(), name='create_item'),
     # update items
     path('update/<int:id>/', views.update_item, name='update_item'),
     # delete
--- First_Project/mysite/food/views.py
+++ First_Project/mysite/food/views.py
@@ -2,38 +2,68 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Item
 from .forms import ItemForm
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from django.template import loader
 
 
 # Create your views here.
-def index(request):
-    item_list = Item.objects.all()
-    context = {
-        'item_list': item_list,
-    }
-    return render(request, 'food/index.html', context)
+# Function based view
+# def index(request):
+#     item_list = Item.objects.all()
+#     context = {
+#         'item_list': item_list,
+#     }
+#     return render(request, 'food/index.html', context)
+
+
+# Class based view using List View, same output as above
+class IndexClassView(ListView):
+    model = Item
+    template_name = 'food/index.html'
+    context_object_name = 'item_list'
 
 
 def item(request):
     return HttpResponse('<h1>This is an item view</h1>')
 
 
-def detail(request, item_id):
-    item = Item.objects.get(pk=item_id)
-    context = {
-        'item': item,
-    }
-    return render(request, 'food/detail.html', context)
+# Function based view
+# def detail(request, item_id):
+#     item = Item.objects.get(pk=item_id)
+#     context = {
+#         'item': item,
+#     }
+#     return render(request, 'food/detail.html', context)
 
 
-def create_item(request):
-    form = ItemForm(request.POST or None)
+# Class based view using Detail View, same output as above
+class FoodDetail(DetailView):
+    model = Item;
+    template_name = 'food/detail.html'
 
-    if form.is_valid():
-        form.save()
-        return redirect('food:index')
+# Function view for create_item
+# def create_item(request):
+#     form = ItemForm(request.POST or None)
+#
+#     if form.is_valid():
+#         form.save()
+#         return redirect('food:index')
+#
+#     return render(request, 'food/item-form.html', {'form': form})
 
-    return render(request, 'food/item-form.html', {'form': form})
+
+# Class based view for create_item
+class CreateItem(CreateView):
+    model = Item
+    # Only fields in form
+    fields = ['item_name', 'item_desc', 'item_price', 'item_image']
+    template_name = 'food/item-form.html'
+
+    def form_valid(self, form):
+        form.instance.user_name = self.request.user
+        return super().form_valid(form)
 
 
 def update_item(request, id):
