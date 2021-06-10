949b634ddb0a46b32b27ce88951b45ff247fadd7
Kyle Jacobs <vaultman765@gmail.com>
2021-06-10 08:41:27 -0400

    admin site updates

--- ecomsite/shop/admin.py
+++ ecomsite/shop/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 from .models import Products, Orders
 
 # Register your models here.
-admin.site.register(Products)
+admin.site.site_header = "E-commerce Site"
+admin.site.site_title = "ABC Shopping"
+admin.site.index_title = "Manage ABC Shopping"
+
+
+class ProductAdmin(admin.ModelAdmin):
+
+    def change_category_to_default(self, request, queryset):
+        queryset.update(category="default")
+
+    change_category_to_default.short_description = 'Default Category'
+    list_display = ('title', 'price', 'discount_price', 'category', 'description', 'image')
+    search_fields = ('title', 'category')
+    actions = ('change_category_to_default',)
+    list_editable = ('price', 'category')
+
+admin.site.register(Products, ProductAdmin)
 admin.site.register(Orders)
